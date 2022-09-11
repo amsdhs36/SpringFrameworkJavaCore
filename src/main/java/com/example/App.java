059cf4909526eab52beb742b04f0ab157c83a63a
amruta <amrutasinghpatil@gmail.com>
2022-09-11 19:46:33 +0530

    With Scanner

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.util.List;
+import java.util.Scanner;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,18 +14,37 @@ public class App
 {
     public static void main( String[] args )
     {
-        @SuppressWarnings("resource")
-		ApplicationContext ac=new ClassPathXmlApplicationContext("Spring.xml");
-        Student s=ac.getBean(Student.class);
-        StudentDAO dao=ac.getBean(StudentDAO.class);
-        int row=dao.insert(s);
-        if(row>0) {
-        	System.out.println("insertion is done");
-        }
-        else {
-        	System.out.println("insertion failed");
-        	
-        }
-        
-    }
+    	 @SuppressWarnings("resource")
+		ApplicationContext ac=new ClassPathXmlApplicationContext("spring.xml");
+         Student s=ac.getBean(Student.class);
+         StudentDAO dao=ac.getBean(StudentDAO.class);
+         try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("enter the id value");
+			s.setSid(sc.nextInt());
+			System.out.println("enter the name");
+			s.setSname(sc.next());
+			System.out.println("enter the email");
+			s.setSemail(sc.next());
+			System.out.println("enter the phono");
+			s.setPhono(sc.nextInt());
+		}
+         int row=dao.insert(s);
+         if(row>0) {
+         	System.out.println("insertion is done");
+         }
+         else {
+         	System.out.println("insertion failed");
+         	
+         }
+         
+         System.out.println();
+         System.out.println();
+         
+         System.out.println("data from the table is ");
+         List<Student> ss=dao.getallstudents();
+         for(Student std:ss) {
+         	System.out.println(std.getSid()+"   "+std.getSname()+"  "+std.getSemail()+"  "+std.getPhono());
+         }
+     }
 }
+
--- src/main/java/com/example/StudentDAO.java
+++ src/main/java/com/example/StudentDAO.java
@@ -1,5 +1,13 @@
 package com.example;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 /*Driver
  * Connection-spring.xml -url,username,pwd
  * statement
@@ -22,4 +30,33 @@ public int insert(Student s) {
 	return temp.update(sql);
 }
 
+/*sql query
+ * statement/preparedStatement
+ * executeQuery->ResultSet(table ref)
+ * */
+
+public List<Student> getallstudents(){
+	String sql="select * from student";
+	return temp.query(sql, new ResultSetExtractor<List<Student>>() {
+
+		public List<Student> extractData(ResultSet rs) throws SQLException, DataAccessException {
+			// TODO Auto-generated method stub
+			ArrayList<Student> list=new ArrayList<Student>();
+			
+			while(rs.next()) {
+				Student s=new Student();
+				s.setSid(rs.getInt(1));
+				s.setSname(rs.getString(2));
+				s.setSemail(rs.getString(3));
+				s.setPhono(rs.getInt(4));
+				list.add(s);
+			}
+			return list;
+		}
+	});
 }
+
+
+}
+
+
